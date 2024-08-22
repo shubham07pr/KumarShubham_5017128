dbae36ac81ba2650965325151d24e44e1bded03a
shubham07pr <138894848+shubham07pr@users.noreply.github.com>
2024-08-22 13:11:47 +0530

    Update UserService.java

--- Week4_Solutions/BookstoreAPI/src/main/java/com/bookstoreapi/service/UserService.java
+++ Week4_Solutions/BookstoreAPI/src/main/java/com/bookstoreapi/service/UserService.java
@@ -3,5 +3,5 @@ package com.bookstoreapi.service;
 import org.springframework.security.core.userdetails.UserDetailsService;
 
 public interface UserService extends UserDetailsService {
-    // Additional methods related to the user can be declared here
+
 }
